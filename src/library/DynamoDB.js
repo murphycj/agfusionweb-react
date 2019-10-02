2bbc7b6470fed27975fcaaa17fc6db388af1886b
murphycj <murphy.charlesj@gmail.com>
2019-10-01 23:03:13 -0400

    method to get sequnces from ddb

--- src/library/DynamoDB.js
+++ src/library/DynamoDB.js
@@ -35,6 +35,29 @@ export class DynamoDB {
     })
   }
 
+  getSequence(gene_id) {
+
+    var params = {
+      TableName: 'agfusion_sequences',
+      Key: {
+        'id': {S: gene_id},
+        'species_release': {S: 'homo_sapiens_94'}
+      }
+    };
+    var self = this;
+
+    return new Promise(function(resolve, reject) {
+      self.ddb.getItem(params, function(err, data) {
+        if (err) {
+          console.log("Error", err);
+          resolve();
+        } else {
+          resolve(data.Item)
+        }
+      });
+    })
+  }
+
   getGeneSynonym(gene_id) {
 
     var params = {
