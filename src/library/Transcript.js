18a9f1bc8597119deef39bb630a1ecc1fd361f72
murphycj <murphy.charlesj@gmail.com>
2019-10-05 22:06:40 -0400

    round protein length, minor formatting

--- src/library/Transcript.js
+++ src/library/Transcript.js
@@ -15,6 +15,11 @@ export class Transcript {
     this.end = parseInt(data.end.N);
     this.strand = strand;
     this.length = this.end - this.start;
+    this.cdsLength = null;
+    this.cdnaLength = null;
+    this.proteinLength = null;
+    this.cds = null;
+    this.exons = null;
 
     this.isProteinCoding = data.is_protein_coding.BOOL || false;
     this.name = data.name.S || this.id;
@@ -74,15 +79,13 @@ export class Transcript {
 
   getLengths() {
     this.cdnaLength = this.exons.map((exon) => exon[1] - exon[0] + 1).reduce((a,b) => a+b);
-    this.cdsLength = 0;
-    this.proteinLength = 0;
 
-    if (this.complete) {
+    if (this.cds) {
       this.cdsLength = this.cds.map((cds_i) => cds_i[1] - cds_i[0] + 1).reduce((a,b) => a+b);
       if ((this.cdsLength % 3) != 0) {
         console.log(this.id);
       }
-      this.proteinLength = this.cdsLength / 3;
+      this.proteinLength = parseFloat((this.cdsLength / 3).toFixed(2));
     }
   }
 
