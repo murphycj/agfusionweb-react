518cef1c15ca1e39ae55ab5c9efa8d0fd3d399c1
murphycj <murphy.charlesj@gmail.com>
2019-10-05 22:07:22 -0400

    add more helpful table display stuff

--- src/components/FusionTable.jsx
+++ src/components/FusionTable.jsx
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { Table, Row, Col, Tag, Switch } from 'antd';
+import { Table, Row, Col, Tag, Switch, Icon, Tooltip, Popover } from 'antd';
 
 class FusionTable extends React.Component {
 
@@ -21,30 +21,80 @@ class FusionTable extends React.Component {
 
   render() {
 
-    const columns = [
-      {
-        title: 'Gene isoforms',
-        dataIndex: 'name',
-        key: 'name'
-      },
-      {
-        title: 'Is protein coding',
-        dataIndex: 'hasProteinCodingPotential',
-        key: 'hasProteinCodingPotential',
-        render: val => (val ? 'Yes' : 'No')
-      },
-      {
-        title: 'Effect',
-        dataIndex: 'effect',
-        key: 'effect'
-      }
-    ];
     if (this.props.fusions) {
       var fusions = Object.keys(this.props.fusions[0].transcripts).map(val => this.props.fusions[0].transcripts[val]);
       if (this.state.onlyCanonical) {
         fusions = fusions.filter(val => val.canonical);
       }
     }
+
+    const columns = [
+      {
+        title: 'Gene isoforms',
+        dataIndex: 'displayData',
+        key: 'name',
+        render: val => {
+          const contentGene1 = (
+            <div>
+              <p><b>ID: </b>{val[0].id}</p>
+              <p><b>Biotype: </b>{val[0].biotype}</p>
+              <p><b>Complete CDS: </b>{val[0].complete ? 'Yes' : 'No'}</p>
+              <p><b>cDNA length: </b>{val[0].cdnaLength} bp</p>
+              <p><b>CDS length: </b>{val[0].cdsLength ? val[0].cdsLength + ' bp' : 'NA'}</p>
+              <p><b>Protein length: </b>{val[0].proteinLength ? val[0].proteinLength + ' aa' : 'NA'}</p>
+            </div>
+          );
+
+          const contentGene2 = (
+            <div>
+              <p><b>ID: </b>{val[1].id}</p>
+              <p><b>Biotype: </b>{val[1].biotype}</p>
+              <p><b>Complete CDS: </b>{val[1].complete ? 'Yes' : 'No'}</p>
+              <p><b>cDNA length: </b>{val[1].cdnaLength} bp</p>
+              <p><b>CDS length: </b>{val[1].cdsLength ? val[1].cdsLength + ' bp' : 'NA'}</p>
+              <p><b>Protein length: </b>{val[1].proteinLength ? val[1].proteinLength + ' aa' : 'NA'}</p>
+            </div>
+          );
+
+          return (
+            <Fragment>
+              <Popover content={contentGene1} title={val[0].name}>
+                <Tag key={val[0].name}>{val[0].name}</Tag>
+              </Popover>
+              <Popover content={contentGene2} title={val[1].name}>
+                <Tag key={val[1].name}>{val[1].name}</Tag>
+              </Popover>
+            </Fragment>
+          )
+        },
+        width: '25%'
+      },
+      {
+        title: 'Has protein coding potential',
+        dataIndex: 'hasProteinCodingPotential',
+        key: 'hasProteinCodingPotential',
+        render: val => (val ? 'Yes' : 'No'),
+        width: '15%'
+      },
+      {
+        title: 'Effect',
+        dataIndex: 'effect',
+        key: 'effect',
+        width: '15%'
+      },
+      {
+        title: '5\' junction location',
+        dataIndex: 'gene1JunctionLoc',
+        key: 'gene1JunctionLoc',
+        width: '15%'
+      },
+      {
+        title: '3\' junction location',
+        dataIndex: 'gene2JunctionLoc',
+        key: 'gene2JunctionLoc',
+        width: '15%'
+      }
+    ];
 
     return (
       this.props.fusions ?
@@ -56,7 +106,10 @@ class FusionTable extends React.Component {
               <Tag>{this.props.fusions[0].gene2.name}</Tag>
             </Col>
             <Col span={6}>
-              Show only canonical: <Switch checked={this.state.onlyCanonical} onChange={this.onChangeCanonical}/>
+              Show only canonical
+              <Tooltip title="By default, only the canonical isoform for each gene in the fusion are shown. Each gene has one canonical isoform, which usually represents the biologically most interesting isoform as well as having the longest coding sequence.">
+                <Icon type="question-circle" />
+              </Tooltip>: <Switch checked={this.state.onlyCanonical} onChange={this.onChangeCanonical}/>
             </Col>
             <Col span={12}>
             </Col>
