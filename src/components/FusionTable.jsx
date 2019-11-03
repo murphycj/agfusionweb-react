2541cfba6b611c887b49e4b657386e9762892546
murphycj <murphy.charlesj@gmail.com>
2019-11-03 09:37:25 -0500

    error handling and display for fusions in table

--- src/components/FusionTable.jsx
+++ src/components/FusionTable.jsx
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
 import { Table, Row, Col, Tag, Switch, Icon, Tooltip, Popover, Select, Divider } from 'antd';
 
-import './FusionTableDetail.css';
+import './FusionTable.css';
 
 const { Option } = Select;
 
@@ -22,10 +22,23 @@ class FusionTableDetail extends React.Component {
 
     const columns = [
       {
-        title: '5\' gene isoform',
+        title: '5\' gene',
         dataIndex: 'displayData',
         key: 'transcript1.id',
         render: val => {
+
+          if (!val[0].id) {
+            return (
+              <p>
+                <Tooltip title={val[2].join(' ')}>
+                  <Icon type="warning" theme="twoTone" twoToneColor="red" />
+                  {val[0]}
+                </Tooltip>
+              </p>
+            )
+            return val[0];
+          }
+
           const contentGene = (
             <div>
               <p><b>ID: </b>{val[0].id}</p>
@@ -48,10 +61,15 @@ class FusionTableDetail extends React.Component {
         width: '20%'
       },
       {
-        title: '3\' gene isoform',
+        title: '3\' gene',
         dataIndex: 'displayData',
         key: 'transcript2.id',
         render: val => {
+
+          if (!val[1].id) {
+            return val[1];
+          }
+
           const contentGene = (
             <div>
               <p><b>ID: </b>{val[1].id}</p>
@@ -81,15 +99,15 @@ class FusionTableDetail extends React.Component {
         width: '15%'
       },
       {
-        title: '5\' junction location',
-        dataIndex: 'gene1JunctionLoc',
-        key: 'gene1JunctionLoc',
+        title: '5\' junction',
+        dataIndex: 'gene1Junction',
+        key: 'gene1Junction',
         width: '15%'
       },
       {
-        title: '3\' junction location',
-        dataIndex: 'gene2JunctionLoc',
-        key: 'gene2JunctionLoc',
+        title: '3\' junction',
+        dataIndex: 'gene2Junction',
+        key: 'gene2Junction',
         width: '15%'
       },
       {
@@ -110,6 +128,7 @@ class FusionTableDetail extends React.Component {
               rowKey="name"
               dataSource={fusionIsoforms}
               columns={columns}
+
               onRow={(record, rowIndex) => {
                 return {
                   onClick: event => onTableRowClickCallback(record)
@@ -126,11 +145,28 @@ class FusionTableDetail extends React.Component {
     var fusionIsoforms = [];
 
     Object.keys(fusions).map(fusion => {
-      return Object.keys(fusions[fusion].transcripts).map(val => {
-        if (fusions[fusion].transcripts[val].canonical) {
-          fusionIsoforms.push(fusions[fusion].transcripts[val]);
-        }
-      });
+
+      if (fusions[fusion].errorMsg) {
+        fusionIsoforms.push({
+          displayData: [
+            fusions[fusion].gene1,
+            fusions[fusion].gene2,
+            fusions[fusion].errorMsg
+          ],
+          transcript1: {id: fusions[fusion].gene1},
+          transcript2: {id: fusions[fusion].gene2},
+          gene1Junction: fusions[fusion].gene1Junction,
+          gene2Junction: fusions[fusion].gene2Junction,
+          effect: 'NA',
+          hasProteinCodingPotential: null,
+        });
+      } else {
+        Object.keys(fusions[fusion].transcripts).map(val => {
+          if (fusions[fusion].transcripts[val].canonical) {
+            fusionIsoforms.push(fusions[fusion].transcripts[val]);
+          }
+        });
+      }
     });
 
     return fusionIsoforms;
