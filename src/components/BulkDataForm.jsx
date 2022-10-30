6e9c98bf7ca10060645246573f92a88beba659e3
murphycj <murphy.jessj@gmail.com>
2022-10-30 14:55:05 -0400

    cleanup

--- src/components/BulkDataForm.jsx
+++ src/components/BulkDataForm.jsx
@@ -1,19 +1,30 @@
-import React, { Fragment } from 'react';
-import {Form, Row, Col, Card, Select, Button, Upload, Tooltip, message, Progress } from 'antd';
-import { InboxOutlined, QuestionCircleOutlined, PaperClipOutlined } from '@ant-design/icons';
+import React from "react";
+import {
+  Form,
+  Row,
+  Col,
+  Card,
+  Select,
+  Button,
+  Upload,
+  Tooltip,
+  message,
+  Progress,
+} from "antd";
+import { InboxOutlined, QuestionCircleOutlined } from "@ant-design/icons";
 
-import './DataForm.css';
+import "./DataForm.css";
 
-import { DynamoDB } from '../library/utils/DynamoDB';
-import { ProcessQuery } from '../library/utils/ProcessQuery';
-import { AVAILABLE_ENSEMBL_SPECIES } from '../library/utils/utils';
-import { UPLOAD_FORMATS } from '../library/utils/utils';
-import { parseUpload } from '../library/upload/base';
-import { ErrorModal } from './ErrorModal.jsx';
+import { ProcessQuery } from "../library/utils/ProcessQuery";
+import { AVAILABLE_ENSEMBL_SPECIES } from "../library/utils/utils";
+import { UPLOAD_FORMATS } from "../library/utils/utils";
+import { parseUpload } from "../library/upload/base";
+import { ErrorModal } from "./ErrorModal.jsx";
 
 const { Dragger } = Upload;
 const helpText = {
-  format: "Generic format has one fusion per row (either TSV or CSV) with this format: gene1,gene1Junction,gene2,gene2Junction (e.g. FGFR2,121487991,CCDC6,59807078)",
+  format:
+    "Generic format has one fusion per row (either TSV or CSV) with this format: gene1,gene1Junction,gene2,gene2Junction (e.g. FGFR2,121487991,CCDC6,59807078)",
 };
 
 export default class BulkDataForm extends React.Component {
@@ -26,14 +37,14 @@ export default class BulkDataForm extends React.Component {
       loading: false,
       uploadedFusionData: null,
       disabled: true,
-      species: 'homo_sapiens_hg38',
+      species: "homo_sapiens_hg38",
       release: 94,
-      format: 'Generic CSV',
+      format: "Generic CSV",
       progress: null,
       showModal: false,
       errorMsg: null,
       fileList: [],
-    }
+    };
   }
 
   componentDidMount() {
@@ -41,7 +52,6 @@ export default class BulkDataForm extends React.Component {
   }
 
   render() {
-
     const {
       loading,
       disabled,
@@ -51,35 +61,42 @@ export default class BulkDataForm extends React.Component {
       showModal,
       uploadedFusionData,
       fileList,
-      errorMsg } = this.state;
+      errorMsg,
+    } = this.state;
 
     var ensembleVersionsOptions = {};
 
-    Object.keys(AVAILABLE_ENSEMBL_SPECIES).map(val => {
-      ensembleVersionsOptions[val] = AVAILABLE_ENSEMBL_SPECIES[val]['ensembl_releases'].map(rel => {
+    Object.keys(AVAILABLE_ENSEMBL_SPECIES).map((val) => {
+      ensembleVersionsOptions[val] = AVAILABLE_ENSEMBL_SPECIES[val][
+        "ensembl_releases"
+      ].map((rel) => {
         return <Select.Option key={rel}>{rel}</Select.Option>;
       });
-    })
-
-    const speciesOption = Object.keys(AVAILABLE_ENSEMBL_SPECIES).map(val => {
-      return <Select.Option key={val}>{AVAILABLE_ENSEMBL_SPECIES[val]['display']}</Select.Option>;
     });
 
-    const uploadFormats = UPLOAD_FORMATS.map(val => {
+    const speciesOption = Object.keys(AVAILABLE_ENSEMBL_SPECIES).map((val) => {
+      return (
+        <Select.Option key={val}>
+          {AVAILABLE_ENSEMBL_SPECIES[val]["display"]}
+        </Select.Option>
+      );
+    });
+
+    const uploadFormats = UPLOAD_FORMATS.map((val) => {
       return <Select.Option key={val}>{val}</Select.Option>;
     });
 
     const props = {
-      name: 'file',
+      name: "file",
       multiple: false,
       customRequest: this._uploadRequest,
       onChange: this._onUploadChange,
       fileList: fileList,
-      iconRender: (file, listType) => {}
+      iconRender: (file, listType) => {},
     };
 
-    const onFinishFailed = errorInfo => {
-      console.log('Failed:', errorInfo);
+    const onFinishFailed = (errorInfo) => {
+      console.log("Failed:", errorInfo);
     };
 
     const layout = {
@@ -93,21 +110,27 @@ export default class BulkDataForm extends React.Component {
         ref={this.formRef}
         onFinish={this._onSubmit}
         onFinishFailed={onFinishFailed}
-        layout={'vertical'}
+        layout={"vertical"}
       >
         <Row gutter={14}>
           <Col xs={24} lg={16}>
             <Card className="dragger" title="Upload" bordered={true}>
-              <Dragger {...props} >
+              <Dragger {...props}>
                 <div className="ant-upload-drag-icon">
                   <InboxOutlined />
                 </div>
-                <div className="ant-upload-text">Click or drag file to this area to upload</div>
+                <div className="ant-upload-text">
+                  Click or drag file to this area to upload
+                </div>
               </Dragger>
             </Card>
           </Col>
           <Col xs={24} lg={8}>
-            <Card className="Card-input" title="Other information" bordered={true}>
+            <Card
+              className="Card-input"
+              title="Other information"
+              bordered={true}
+            >
               <Form.Item
                 label={
                   <div>
@@ -119,18 +142,16 @@ export default class BulkDataForm extends React.Component {
                 }
                 name="upload_format"
                 className="form-item"
-                rules={[{required: true}]}
+                rules={[{ required: true }]}
               >
-                <Select style={{ minWidth: '10rem' }}>
-                  {uploadFormats}
-                </Select>
+                <Select style={{ minWidth: "10rem" }}>{uploadFormats}</Select>
               </Form.Item>
 
               <Form.Item
                 label="Species"
                 name="species"
                 className="form-item"
-                rules={[{required: true}]}
+                rules={[{ required: true }]}
               >
                 <Select onChange={this._handleSpeciesChange}>
                   {speciesOption}
@@ -141,7 +162,7 @@ export default class BulkDataForm extends React.Component {
                 label="Ensembl release"
                 name="release"
                 className="form-item"
-                rules={[{required: true}]}
+                rules={[{ required: true }]}
               >
                 <Select onChange={this._handleReleaseChange}>
                   {ensembleVersionsOptions[species]}
@@ -157,17 +178,27 @@ export default class BulkDataForm extends React.Component {
             htmlType="submit"
             type="primary"
             className="button"
-            loading={loading}>
+            loading={loading}
+          >
             Submit
           </Button>
-          <Button type="default" className="button" onClick={this._clearData}>Clear</Button>
+          <Button type="default" className="button" onClick={this._clearData}>
+            Clear
+          </Button>
         </Row>
         <Row>
-          {progress ? <Progress className="progress-bar" percent={progress}/> : null}
+          {progress ? (
+            <Progress className="progress-bar" percent={progress} />
+          ) : null}
         </Row>
-        {showModal ? <ErrorModal errorMsg={errorMsg} closeModalCallback={this._closeModal}/> : null}
+        {showModal ? (
+          <ErrorModal
+            errorMsg={errorMsg}
+            closeModalCallback={this._closeModal}
+          />
+        ) : null}
       </Form>
-    )
+    );
   }
 
   _handleSpeciesChange = (value) => {
@@ -177,39 +208,37 @@ export default class BulkDataForm extends React.Component {
         value: value,
       },
       {
-        name: 'release',
-        value: AVAILABLE_ENSEMBL_SPECIES[value]['default_release']
-      }
+        name: "release",
+        value: AVAILABLE_ENSEMBL_SPECIES[value]["default_release"],
+      },
     ]);
-  }
+  };
 
   _handleReleaseChange = (value) => {
     this.formRef.current.setFields([
       {
-        name: 'release',
-        value: value
-      }
+        name: "release",
+        value: value,
+      },
     ]);
-  }
+  };
 
-  _onUploadChange = ({file, fileList, e}) => {
-
+  _onUploadChange = ({ file, fileList, e }) => {
     if (fileList.length > 1) {
       fileList.shift();
     }
 
-    if (file.status === 'done') {
+    if (file.status === "done") {
       message.success(`${fileList[0].name} file uploaded successfully.`);
-    } else if (file.status === 'error') {
+    } else if (file.status === "error") {
       this.setState({
         fileList: [],
       });
       message.error(`${fileList[0].name} file upload failed.`);
     }
-  }
+  };
 
-  _uploadRequest = ({file, onSuccess, onError}) => {
-
+  _uploadRequest = ({ file, onSuccess, onError }) => {
     const { format } = this.state;
 
     setTimeout(async () => {
@@ -238,33 +267,32 @@ export default class BulkDataForm extends React.Component {
     this.setState({
       showModal: false,
     });
-  }
+  };
 
   _handleFormatChange = (value) => {
     this.setState({
       format: value,
     });
-  }
+  };
 
   _resetForm = () => {
     this.formRef.current.setFields([
       {
         name: "species",
-        value: 'homo_sapiens_hg38',
+        value: "homo_sapiens_hg38",
       },
       {
-        name: 'release',
-        value: 94
+        name: "release",
+        value: 94,
       },
       {
-        name: 'upload_format',
-        value: 'Generic CSV'
-      }
+        name: "upload_format",
+        value: "Generic CSV",
+      },
     ]);
-  }
+  };
 
   _clearData = () => {
-
     this._resetForm();
 
     this.setState({
@@ -274,26 +302,24 @@ export default class BulkDataForm extends React.Component {
       progress: null,
       showModal: false,
       errorMsg: null,
-      fileList: []
+      fileList: [],
     });
     this.props.onClearCallback();
-  }
+  };
 
   _setLoading = () => {
-
     const { loading } = this.state;
 
     this.setState({
       loading: !loading,
-    })
-  }
+    });
+  };
 
   _onSubmit = async (values) => {
-
     const { query, uploadedFusionData, progress } = this.state;
     const { rollbar } = this.props;
 
-    const species = AVAILABLE_ENSEMBL_SPECIES[values.species]['species'];
+    const species = AVAILABLE_ENSEMBL_SPECIES[values.species]["species"];
     const speciesRelease = `${species}_${values.release}`;
 
     var fusions = [];
@@ -301,7 +327,6 @@ export default class BulkDataForm extends React.Component {
     this._setLoading();
 
     for (var i = 0; i < uploadedFusionData.length; i++) {
-
       var fusion = uploadedFusionData[i];
       var gene1EnsemblIds = null;
       var gene1Data = null;
@@ -330,23 +355,30 @@ export default class BulkDataForm extends React.Component {
         if (gene1Data.length !== 0) {
           // get genes where junction occurs in
 
-          gene1DataFinal = []
+          gene1DataFinal = [];
 
           for (var j = 0; j < gene1Data.length; j++) {
             if (gene1Data[j].contains(fusion.gene1Pos)) {
-              gene1Data[j] = await query._getSequenceData(gene1Data[j], speciesRelease);
+              gene1Data[j] = await query._getSequenceData(
+                gene1Data[j],
+                speciesRelease
+              );
               gene1DataFinal.push(gene1Data[j]);
             }
           }
 
           if (gene1DataFinal.length === 0) {
-            errorMsg.push('Junction not within the 5\' gene. Check the selected genome.');
+            errorMsg.push(
+              "Junction not within the 5' gene. Check the selected genome."
+            );
           }
         } else {
-          errorMsg.push('Unknown 5\' gene gene. Check your spelling and genome.');
+          errorMsg.push(
+            "Unknown 5' gene gene. Check your spelling and genome."
+          );
         }
       } else {
-        errorMsg.push('Unknown 5\' gene gene. Check your spelling and genome.');
+        errorMsg.push("Unknown 5' gene gene. Check your spelling and genome.");
       }
 
       // validate gene 2
@@ -361,34 +393,42 @@ export default class BulkDataForm extends React.Component {
         if (gene2Data.length !== 0) {
           // get genes where junction occurs in
 
-          gene2DataFinal = []
+          gene2DataFinal = [];
 
           for (var j = 0; j < gene2Data.length; j++) {
             if (gene2Data[j].contains(fusion.gene2Pos)) {
-              gene2Data[j] = await query._getSequenceData(gene2Data[j], speciesRelease);
+              gene2Data[j] = await query._getSequenceData(
+                gene2Data[j],
+                speciesRelease
+              );
               gene2DataFinal.push(gene2Data[j]);
             }
           }
 
           if (gene2DataFinal.length === 0) {
-            errorMsg.push('Junction not within the 3\' gene. Check the selected genome.');
+            errorMsg.push(
+              "Junction not within the 3' gene. Check the selected genome."
+            );
           }
         } else {
-          errorMsg.push('Unknown 3\' gene gene. Check your spelling and genome.');
+          errorMsg.push(
+            "Unknown 3' gene gene. Check your spelling and genome."
+          );
         }
       } else {
-        errorMsg.push('Unknown 3\' gene gene. Check your spelling and genome.');
+        errorMsg.push("Unknown 3' gene gene. Check your spelling and genome.");
       }
 
       if (errorMsg.length > 0) {
         fusions.push({
-          gene1: fusion.gene1.join(','),
-          gene1Data: fusion.gene2.join(','),
+          gene1: fusion.gene1.join(","),
+          gene1Data: fusion.gene2.join(","),
           gene1Junction: fusion.gene1Pos,
-          gene2: fusion.gene2.join(','),
-          gene2Data: fusion.gene2.join(','),
+          gene2: fusion.gene2.join(","),
+          gene2Data: fusion.gene2.join(","),
           gene2Junction: fusion.gene2Pos,
-          errorMsg: errorMsg});
+          errorMsg: errorMsg,
+        });
       } else {
         fusions.push({
           gene1: gene1EnsemblIds,
@@ -397,7 +437,8 @@ export default class BulkDataForm extends React.Component {
           gene2: gene2EnsemblIds,
           gene2Data: gene2DataFinal,
           gene2Junction: fusion.gene2Pos,
-          errorMsg: errorMsg});
+          errorMsg: errorMsg,
+        });
       }
 
       this.setState({
@@ -416,5 +457,5 @@ export default class BulkDataForm extends React.Component {
       });
     }, 1000);
     this.props.onSubmitCallback(fusions);
-  }
+  };
 }
